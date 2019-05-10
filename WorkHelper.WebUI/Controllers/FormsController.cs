3a7d62da1ce2adc3ab012c6d9495d7356ed7c406
Alexey Anisenkov <a.anisenkov@outlook.com>
2019-05-10 12:05:56 +0300

    Небольшие правки в каркас

---
+++ WorkHelper.WebUI/Controllers/FormsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkHelper.WebUI.Controllers
+{
+    public class FormsController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
